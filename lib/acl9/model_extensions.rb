62135ed7e8eb2ae14978e88bcaf3227143892541
Jordan Brock <jordanbrock@gmail.com>
2011-03-27 16:56:11 +0800

    Removing join table

--- lib/acl9/model_extensions.rb
+++ lib/acl9/model_extensions.rb
@@ -38,7 +38,7 @@ module Acl9
         join_table = options[:join_table_name] || Acl9::config[:default_join_table_name] ||
                     join_table_name(undecorated_table_name(self.to_s), undecorated_table_name(role))
 
-        has_and_belongs_to_many assoc, :class_name => role, :join_table => join_table
+        has_and_belongs_to_many assoc, :class_name => role#, :join_table => join_table
 
         cattr_accessor :_auth_role_class_name, :_auth_subject_class_name,
                        :_auth_role_assoc_name
@@ -131,8 +131,8 @@ module Acl9
                      join_table_name(undecorated_table_name(self.to_s), undecorated_table_name(subject))
 
         has_and_belongs_to_many subject.demodulize.tableize.to_sym,
-          :class_name => subject,
-          :join_table => join_table
+          :class_name => subject#,
+          #:join_table => join_table
 
         belongs_to :authorizable, :polymorphic => true
       end
